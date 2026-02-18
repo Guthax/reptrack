5d899bca02405c91f5da8372dbcdf1d3d91d7417
Jurriaan <jurrbuitenweg@gmail.com>
2026-02-18 19:23:04 +0100

    delete everything

--- reptrack/android/app/src/main/kotlin/com/example/reptrack/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.reptrack
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- reptrack/test/db/mongodb_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-void main() {
-  test('simple mongodb example', () async {
-    //final config = Configuration.local([WorkoutSchedule.schema, Workout.schema, Exercise.schema, TrainingSession.schema, SessionExercise.schema]);
-    //final realm = Realm(config);
-    //realm.close();
-    expect(0, 0);
-  });
-}
